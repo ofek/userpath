679daf6ce8aab006bf644d9d04c17b651bc9407d
ofek <ofekmeister@gmail.com>
2017-09-20 22:27:18 -0400

    another test

--- tests/test_core.py
+++ tests/test_core.py
@@ -5,8 +5,14 @@ import userpath
 
 
 def test_prepend():
-    path = urlsafe_b64encode(urandom(5)).decode()
-    assert not userpath.in_current_path(path)
-    userpath.prepend(path)
-    print(userpath.get_new_path())
-    assert userpath.in_new_path(path)
+    location = urlsafe_b64encode(urandom(5)).decode()
+    assert not userpath.in_current_path(location)
+    assert userpath.prepend(location)
+    assert userpath.in_new_path(location)
+
+
+def test_append():
+    location = urlsafe_b64encode(urandom(5)).decode()
+    assert not userpath.in_current_path(location)
+    assert userpath.append(location)
+    assert userpath.in_new_path(location)
